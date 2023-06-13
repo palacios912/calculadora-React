7cb426eba3d7e9b6178a15a55c07843d0b3666fd
lucas palacios <lpalacios168@alumnos.frh.utn.edu.ar>
2023-06-12 22:48:40 -0300

    carga

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@
 import './App.css';
 import Boton from './componentes/Boton';
 import zero from './imagenes/imagen-zero.jpg';
+import Pantalla from './componentes/Pantalla.js';
+import BotonClear from './componentes/BotonClear';
+import {useState} from 'react';
 function App() {
+  const [input,setInput] = useState('0');
+  const agregarInput = valor =>{
+    setInput(input + valor);
+  };
   return (
     <div className="App">
       <div className='zero-logo-contenedor'>
@@ -14,15 +21,39 @@ function App() {
        
       </div>
       <div className='contenedor-calculadora'>
+        
+        <Pantalla input={input} />
+        
         <div className='fila'>
           <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>=</Boton>
+          <Boton>/</Boton>
+        </div>
         
-        <div className='fila'></div>
+        <div className='fila'>
+          <BotonClear>
+            Clear
+          </BotonClear>
+        </div>
       </div>
     </div>
   );
